e8a539e188acdd6d9e52b27b957c3e2b2f363e5f
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-07-12 16:37:52 +0200

    Basic player class

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+const KEYS = {
+	A: 'KeyA',
+	D: 'KeyD',
+	SPACE: 'KeySpace'
+}
+
+const DIRECTION = {
+	LEFT: 0,
+	RIGHT: 1,
+	NONE: 10
+}
+
+class Player {
+	constructor(ctx, canvas) {
+		this.ctx = ctx
+		this.canvas = canvas
+		this.height = 20
+		this.width = 20
+		this.y = this.canvas.height - (this.height + 20)
+		this.x = (this.canvas.width / 2) - (this.width / 2)
+		this.direction = DIRECTION.NONE
+
+		window.addEventListener('keydown', (e) => {
+			if (e.code == KEYS.A) this.direction = DIRECTION.LEFT
+			if (e.code == KEYS.D) this.direction = DIRECTION.RIGHT
+		})
+		window.addEventListener('keyup', (e) => {
+			if (e.code == KEYS.A && this.direction == DIRECTION.LEFT) this.direction = DIRECTION.NONE
+			if (e.code == KEYS.D && this.direction == DIRECTION.RIGHT) this.direction = DIRECTION.NONE
+		})
+	}
+	update() {
+		if (this.x < 0) this.x = 0
+		if (this.x > this.canvas.width - this.width) this.x = this.canvas.width - this.width
+		if (this.direction == DIRECTION.LEFT) this.x -= 2
+		if (this.direction == DIRECTION.RIGHT) this.x += 2
+	}
+	draw() {
+		this.ctx.fillRect(this.x, this.y, this.width, this.height)
+	}
+}
+
 class Game {
 	constructor() {
 		this.canvas = document.getElementById('canvas')
@@ -6,25 +48,27 @@ class Game {
 		this.canvas.width = document.documentElement.clientWidth
 		this.canvas.height = document.documentElement.clientHeight
 
-		this.started = false
+		this.player = new Player(this.ctx, this.canvas)
+
+		this.started = true
 
 		setInterval(() => {
-			if (this.started) {
-				this.update()
-				this.draw()
-			}
+			if (this.started) this.update()
+			this.draw()
 		}, 0)
 	}
-
-	update() {}
 	clear() {
 		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+	}
+	update() {
+		this.player.update()
 	}
 	draw() {
 		this.clear()
 		this.ctx.fillStyle = '#0f0f0f'
 		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
 		this.ctx.fillStyle = '#FFFFFF'
+		this.player.draw()
 	}
 }
 
