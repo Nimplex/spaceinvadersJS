f4c878bccbf55cc0d9e6aef4d3007809bf8f7868
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-07-12 17:04:45 +0200

    Bullets

--- js/script.js
+++ js/script.js
@@ -7,13 +7,34 @@ const KEYS = {
 const DIRECTION = {
 	LEFT: 0,
 	RIGHT: 1,
+	UP: 2,
 	NONE: 10
 }
 
-class Player {
-	constructor(ctx, canvas) {
+class Bullet {
+	constructor(ctx, canvas, game) {
 		this.ctx = ctx
 		this.canvas = canvas
+		this.game = game
+		this.height = 5
+		this.width = 5
+		this.y = this.game.player.y - this.height
+		this.x = this.game.player.x + (this.game.player.width - this.width) / 2
+		this.direction = DIRECTION.UP
+	}
+	update() {
+		if (this.direction == DIRECTION.UP) this.y -= 1
+	}
+	draw() {
+		this.ctx.fillRect(this.x, this.y, this.width, this.height)
+	}
+}
+
+class Player {
+	constructor(ctx, canvas, game) {
+		this.ctx = ctx
+		this.canvas = canvas
+		this.game = game
 		this.height = 20
 		this.width = 20
 		this.y = this.canvas.height - (this.height + 20)
@@ -23,6 +44,7 @@ class Player {
 		window.addEventListener('keydown', (e) => {
 			if (e.code == KEYS.A) this.direction = DIRECTION.LEFT
 			if (e.code == KEYS.D) this.direction = DIRECTION.RIGHT
+			if (e.code == KEYS.SPACE) this.game.bullets.push(new Bullet(this.ctx, this.canvas, this.game))
 		})
 		window.addEventListener('keyup', (e) => {
 			if (e.code == KEYS.A && this.direction == DIRECTION.LEFT) this.direction = DIRECTION.NONE
@@ -48,7 +70,8 @@ class Game {
 		this.canvas.width = document.documentElement.clientWidth
 		this.canvas.height = document.documentElement.clientHeight
 
-		this.player = new Player(this.ctx, this.canvas)
+		this.bullets = []
+		this.player = new Player(this.ctx, this.canvas, this)
 
 		this.started = true
 
@@ -62,6 +85,11 @@ class Game {
 	}
 	update() {
 		this.player.update()
+		this.bullets.forEach(bullet => {
+			const bulletIndex = this.bullets.indexOf(bullet)
+			if (bullet.y <= 0) this.bullets.splice(bulletIndex, bulletIndex + 1)
+			bullet.update()
+		})
 	}
 	draw() {
 		this.clear()
@@ -69,6 +97,7 @@ class Game {
 		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
 		this.ctx.fillStyle = '#FFFFFF'
 		this.player.draw()
+		this.bullets.forEach(bullet => bullet.draw())
 	}
 }
 
