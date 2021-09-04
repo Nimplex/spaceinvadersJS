ed2b70eb3fa842fbae9aecc10fb6db2f0146319b
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-09-04 16:44:22 +0200

    preparing new version

---
+++ js/newscript.js
@@ -0,0 +1,33 @@
+const { keys, directions, types } = {
+	keys: {
+		left: 'KeyA',
+		rigth: 'KeyD',
+		space: 'Space'
+	},
+	directions: {
+		left: 1,
+		right: 2,
+		up: 3,
+		down: 4,
+		none: 0,
+	},
+	types: {
+		enemy: 0,
+		player: 1,
+	}
+}
+
+const menu = document.getElementById('menu')
+const gameOver = document.getElementById('menu_over')
+
+class Bullet {
+
+} // TODO:...
+
+class Character {
+
+} // TODO:...
+
+class Game {
+
+} // TODO:...
