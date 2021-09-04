68dda797db66870e2776c62041a7a7be9a96219e
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-09-04 17:25:09 +0200

    main menu logic

--- js/newscript.js
+++ js/newscript.js
@@ -1,3 +1,5 @@
+// --------------------------------------------------- //
+
 const { keys, directions, types } = {
 	keys: {
 		left: 'KeyA',
@@ -17,13 +19,82 @@ const { keys, directions, types } = {
 	},
 }
 
+// --------------------------------------------------- //
+
 const menu = document.getElementById('menu')
-const gameOver = document.getElementById('menu_over')
+const menuOver = document.getElementById('menu_over')
+const menuStart = document.getElementById('menu_start')
+
+// --------------------------------------------------- //
+
+// Play game
 const playButton = document.getElementById('playButton')
+// Open shop
 const shopButton = document.getElementById('shopButton')
+// Retry game
+const retryButton = document.getElementById('retryButton')
+// Back to main menu
+const backButton = document.getElementById('backButton')
+
+// --------------------------------------------------- //
 
 class Bullet {} // TODO:...
 
 class Character {} // TODO:...
 
-class Game {} // TODO:...
+class Game {
+	constructor() {
+		this.started = false
+		this.over = false
+
+		menuStart.style.display = 'none'
+		menuOver.style.display = 'none'
+		menu.style.display = 'none'
+
+		playButton.onclick = () => {
+			this.started = true
+		}
+		retryButton.onclick = () => {
+			this.reset()
+			this.started = true
+		}
+		backButton.onclick = () => {
+			this.started = false
+			this.over = false
+		}
+
+		setInterval(() => {
+			if (this.started) {
+				menuStart.style.display = 'none'
+				menuOver.style.display = 'none'
+				menu.style.display = 'none'
+				this.update()
+			}
+			if (this.over) {
+				menu.style.display = 'flex'
+				menuOver.style.display = 'block'
+				menuStart.style.display = 'none'
+			}
+			if (!this.started && !this.over) {
+				menu.style.display = 'flex'
+				menuStart.style.display = 'block'
+				menuOver.style.display = 'none'
+			}
+		}, 1)
+	}
+
+	update() {
+		console.log('Update')
+	}
+	draw() {}
+	reset() {
+		this.over = false
+		this.started = false
+	}
+}
+
+// --------------------------------------------------- //
+
+window.onload = () => new Game()
+
+// --------------------------------------------------- //
